db7b68f95d421a1ddaf4dcc205eb66facda1aae4
gyeongseok <5rocksdev@gmail.com>
2024-03-19 01:16:34 +0900

    Initial commit

---
+++ app/src/main/java/com/gyeongseok/signup_gyeongseok/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gyeongseok.signup_gyeongseok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
